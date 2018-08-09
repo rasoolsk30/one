a62ac2fab060843adc202c477caaef50a5278a89
rasool <rasools27@gmail.com>
2018-08-09 14:42:14 +0000

    again modified

--- java.java
+++ java.java
@@ -26,3 +26,17 @@ class PrimeCheck
 	   System.out.println(num + " is not a Prime Number");
    }
 }
+  temp=num%i;
+	   if(temp==0)
+	   {
+	      isPrime=false;
+	      break;
+	   }
+	}
+	//If isPrime is true then the number is prime else not
+	if(isPrime)
+	   System.out.println(num + " is a Prime Number");
+	else
+	   System.out.println(num + " is not a Prime Number");
+   }
+}
